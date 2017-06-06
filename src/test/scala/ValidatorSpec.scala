c32566a953160d63a616a88812636343e71d2142
Neel Smith <dnsmith.neel@gmail.com>
2017-06-06 16:45:34 -0400

    Comment out test needing mock

--- src/test/scala/ValidatorSpec.scala
+++ src/test/scala/ValidatorSpec.scala
@@ -12,22 +12,23 @@ import net.liftweb.json._
 class ValidatorSpec extends FlatSpec {
 
   val madeUpURL = new URL("http://cite-architecture.github.io/TOTALLY/MADE/UP/SERVICE/URL")
-
+  val validator = Validator(madeUpURL)
 
   "A Validator" should "be constructed from a URL" in {
-    val validator = Validator(madeUpURL)
     validator match {
       case v: Validator => assert(true)
       case _ => fail("Should have created a Validator")
     }
   }
 
-  it should "validate against a test suite defined by parsed JSON" in pending
-  /*
+  it should "validate against a test suite defined by parsed JSON" in pending/*
+
     val testSrc = Source.fromFile("src/test/resources/texts-tests-1.0.DRAFT.json")
-    val jsonText = testSrc.getLines.mkString("\n")
-
-    println(validator.validate(parse(jsonText)))
-  }*/
-
+    val json = parse(testSrc.getLines.mkString("\n"))
+    println(json)
+    val tests = validator.validate(json)
+    println("TESTS: " + tests)
+    //println(validator.validate(parse(jsonText)))
+  }
+*/
 }
