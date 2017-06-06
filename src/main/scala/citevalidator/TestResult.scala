3957457e04902d9f565766996cbe200273726d17
Neel Smith <dnsmith.neel@gmail.com>
2017-06-06 07:04:43 -0400

    Add template for TestResult

---
+++ src/main/scala/citevalidator/TestResult.scala
@@ -0,0 +1,13 @@
+package edu.holycross.shot.citevalidator
+
+import edu.holycross.shot.cite._
+
+
+/** Results of running a test.
+*
+* @param testId Test run.
+* @param success True if test succeeded.
+* @param message Human-readable message.
+*/
+case class TestResult(testId: Cite2Urn, success: Boolean, message: String) {
+}
